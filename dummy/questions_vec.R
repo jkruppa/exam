42b3646fdf4967dbbb395ed1b815dc56d6e4c762
Jochen Kruppa <jkruppa@users.noreply.github.com>
2023-12-26 07:10:55 +0100

    added text

--- dummy/questions_vec.R
+++ dummy/questions_vec.R
@@ -1,3 +1,3 @@
-wanted_questions_lst <- list(mc = c(1, 3, 4, 5, 12,
-                                    13, 23, 34, 40, 22),
-                             text = c(47, 49, 56, 78, 81, 90, 51))
+wanted_questions_lst <- list(mc = c(1, 6, 13, 22, 29,
+                                    33, 35, 38, 43, 46),
+                             text = c(49, 62, 68, 73, 80, 93, 54))
