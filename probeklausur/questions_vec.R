57b76d64a9985c4ed5c815d0cc6c3d0ce4e5657e
Jochen Kruppa-Scheetz <j.kruppa@hs-osnabrueck.de>
2026-05-19 19:50:25 +0200

    added

--- probeklausur/questions_vec.R
+++ probeklausur/questions_vec.R
@@ -1,12 +1,18 @@
 ## bio data science
-#wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
-#                                    19, 21, 22, 29, 33),
-#                             text = c(52, 55, 62, 
-#                                      65, 70, 74, 
-#                                      120))
-
-## mathematik und statistik
 wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
                                     19, 21, 22, 29, 33),
-                             text = c(52, 62, 65, 
-                                      99, 100, 108, 114))
+                             text = c(52, 55, 62, 
+                                      65, 70, 74, 
+                                      120))
+
+## mathematik und statistik
+#wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
+#                                    19, 21, 22, 29, 33),
+#                             text = c(52, 62, 65, 
+#                                      99, 100, 108, 114))
+
+## mathematik und statistik für bbv
+#wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
+#                                    19, 21, 22, 29, 33),
+#                             text = c(52, 62, 65, 68, 74,
+#                                      99, 115))
