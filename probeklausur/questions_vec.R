46e7becde0d59508bd6ff5c373c4918b7e48e2fd
Jochen Kruppa-Scheetz <j.kruppa@hs-osnabrueck.de>
2026-09-13 20:18:20 +0200

    added

--- probeklausur/questions_vec.R
+++ probeklausur/questions_vec.R
@@ -2,14 +2,13 @@
 wanted_questions_lst <- list(mc = c(6, 7, 9, 13, 14,
                                     19, 21, 22, 29, 33),
                              text = c(52, 55, 63, 
-                                      76, 70, 79, 
-                                      120))
+                                      73, 82, 118))
 
 ## mathematik und statistik
 #wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
-#                                    19, 21, 22, 29, 33),
-#                             text = c(52, 62, 65, 
-#                                      99, 100, 108, 114))
+ #                                   19, 21, 22, 29, 33),
+  #                           text = c(52, 60, 63, 
+   #                                   97, 106, 112))
 
 ## mathematik und statistik für bbv
 #wanted_questions_lst <- list(mc = c(5, 6, 7, 13, 14,
